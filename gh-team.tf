9ce28a231ea904706b220e0ca86196b1d486571b
ABGEO <takalandzet@gmail.com>
2025-06-13 18:47:45 +0400

    chore: trigger digger

--- gh-team.tf
+++ gh-team.tf
@@ -1,5 +1,5 @@
 resource "github_team" "sdk_maintainers" {
   name        = "SDK Maintainers"
-  description = "Maintains and improves SDKs. Reviews PRs, manages issues, and ensures alignment with API standards."
+  description = "Maintains and improves SDKs. Reviews PRs, manages issues, and ensures alignment with API standards"
   privacy     = "closed"
 }
